b3ea6cf5b37b8f92b4d9ad8eef7eeae89d2c0631
Raju Bhattarai <rbhattarai525@gmail.com>
2026-05-03 22:22:23 +0545

    feat(script): Added new comment on  indirect_referencing.sh

--- basics/indirect_referencing.sh
+++ basics/indirect_referencing.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#  Bracket notation for positional parameters leads to a fairly 
+#+ simple way of referencing the last argument passed to a script
+#+ son the command-line. 
+#+ This  also requires indirect referencing.
+
 args=$#            # Number of args passed
 lastarg=${!args}   #
 # Note: This is an *indirect reference* to $args ...
